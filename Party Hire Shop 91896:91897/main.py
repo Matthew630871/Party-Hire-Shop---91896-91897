3c8e34037845531b127977306a8e28310f578d9d
Matthew630871 <mm23030@mbc.school.nz>
2026-06-16 10:22:54 +1200

    Added Code Comments to layout main.py

---
+++ Party Hire Shop 91896:91897/main.py
@@ -0,0 +1,51 @@
+import tkinter 
+
+# Data lists
+
+
+
+# Helper Functions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ---------------------------- Define TKinter GUI ---------------------------- 
+
+
+# ----- Add app Name label ------ 
+
+ 
+# ----- Add Customer label and entry box -----  
+
+
+# ----- Add Receipt label and entry box -----  
+
+
+# ----- Add Item hired label and dropdown box -----  
+
+
+# ----- Add Number Hired label and entry box -----  
+
+
+# ----- Add Sumbit Button -----  
+
+
+# ----- Add Return items label -----  
+
+
+# ----- Add Customer name label and entry box -----  

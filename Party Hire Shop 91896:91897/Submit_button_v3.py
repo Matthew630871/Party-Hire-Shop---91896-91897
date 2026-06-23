b7878ac4798e16000871af899cd8acd68928fa09
Matthew630871 <mm23030@mbc.school.nz>
2026-06-24 09:19:50 +1200

    Coded in hard cap on item amount hired

--- Party Hire Shop 91896:91897/Submit_button_v3.py
+++ Party Hire Shop 91896:91897/Submit_button_v3.py
@@ -2,9 +2,15 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 
+#Program constants
+hireable_items= ["Blank_1","Blank_2","Blank_3"]
+Hired_cap = 500
 # Data lists
+
 customer_names=[]
 receipt_numbers=[]
+items_hired=[]
+amount_hired=[]
 
 # Helper Functions
 
@@ -12,37 +18,61 @@ def Submit():
     first_name = first_name_entry.get().strip()
     last_name = last_name_entry.get().strip()
     receipt = receipt_entry.get().strip()
-
+    item = hired.get()
+    amount = hired_num.get().strip()
     
-    # Checking if names are missing
+    # 1. Checking if names are missing
     if not first_name or not last_name:
         messagebox.showwarning("Input Error", "Both First Name and Last Name are required!")
         return
     
-    # Checking if the receipt number is missing or not an integer
+    # 2. Checking if the receipt number is missing or not an integer
     if not receipt:
         messagebox.showwarning("Input Error", "Receipt Number is required")
         return
-    
     try:
         new_num = int(receipt)
     except ValueError:
         messagebox.showerror("Error", "Receipt number must be an integer.")
         return
 
-    # If it passes all checks, save the data
-    full_name = f"{first_name}{last_name}"
+    # 3. Checking if a item was selected from dropdown
+    if not item:
+        messagebox.showwarning("Input Error","Please select an item to hire!")
+        return
+    
+    # 4. Checking if the amount of missing or not a valid positive integer
+    if not amount:
+        messagebox.showwarning("Input Error", "Amount Hired is required")
+        return
+    try:
+        new_amount = int(amount)
+        if new_amount <= 0:
+            messagebox.showerror("Error", "Amount hired must be greater than 0.")
+            return
+        if new_amount > Hired_cap:
+            messagebox.showerror("Error", f"Maximum of {Hired_cap} items allowed.")
+            return
+    except ValueError:
+        messagebox.showerror("Error", "Amount hired must be a whole number.")
+        return
+
+    # 5. If it passes all checks, save the data
+    full_name = f"{first_name} {last_name}"
     customer_names.append(full_name)
     receipt_numbers.append(new_num)
+    items_hired.append(item)
+    amount_hired.append(new_amount)
 
-    print(customer_names) # for testing
-    print(receipt_numbers)# for testing
-
-    # clearing boxes after a seccessful save 
+    # Printing out for testing
+    print(f"Saved entry: {full_name}, Receipt: {new_num}, Item: {item}, Qty: {new_amount}")
+    
+    # 6. clearing boxes after a seccessful save 
     first_name_entry.delete(0, tk.END)
     last_name_entry.delete(0, tk.END)
     receipt_entry.delete(0, tk.END)
-
+    hired.set('')
+    hired_num.delete(0, tk.END)
 
 
 
@@ -85,7 +115,7 @@ receipt_entry.grid(row=2, column=1)
 
 # ----- Item hired label and dropdown box -----  
 ttk.Label(root, text="Item Hired").grid(row=3, column=0,)
-hired = ttk.Combobox(root, values=["blank","blank"], state="read only")
+hired = ttk.Combobox(root, values=hireable_items, state="read only")
 hired.grid(row=3, column=1)
 
 # ----- Number Hired label and entry box -----  
